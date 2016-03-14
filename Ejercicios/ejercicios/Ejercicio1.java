673373c1fdf297e67f0dfbdb1141af0ab3ce1bf4
Alvaro <alvarojimenez95@hotmail.com>
2016-03-14 14:39:41 +0100

    13/03/2016

--- Ejercicios/ejercicios/Ejercicio1.java
+++ Ejercicios/ejercicios/Ejercicio1.java
@@ -1,9 +1,21 @@
 package ejercicios;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 public class Ejercicio1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		File outFile = new File("salidaEjercicios/ejercicioFileOutputStream.txt");
+		try(FileOutputStream out = new FileOutputStream(outFile);) {
+			for (int i = 20; i <= 40; i++) {
+				out.write(i);
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 
 	}
 
